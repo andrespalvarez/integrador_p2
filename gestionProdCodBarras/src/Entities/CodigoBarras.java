bf0b6a8b2ce62b4ca000d2c909db36988b72a49c
andrespalvarez <andrespalvarez@hotmail.com>
2026-01-21 18:29:11 -0300

    se reemplazaron todos el scr con los archivos de Emiliano de Ennero

--- gestionProdCodBarras/src/Entities/CodigoBarras.java
+++ gestionProdCodBarras/src/Entities/CodigoBarras.java
@@ -1,31 +1,17 @@
 package Entities;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
-/**
- * Entidad que representa un codigo de barras en el sistema.
- * Hereda de Base para obtener id y eliminado.
- *
- * Relación con Producto:
- * - Un Producto puede tener 0 o 1 Codigo de Barras
- * - Un Codigo de barras puede estar asociado UN UNICO codigo de barras
- *
- * Tabla BD: codigobarras
- * Campos:
- * id INT AUTO_INCREMENT PRIMARY KEY,
- * eliminado bool,
- * tipo enum ('EAN13','EAN8','UPC') NOT NULL,
- * valor varchar(20) NOT NULL UNIQUE,
- * fechaAsignacion LocalDate,
- * observaciones varchar(255) 
- */
 public class CodigoBarras extends Base {
-    
-    private String valor;              // NOT NULL
-    private String tipo;               // EAN8, EAN13, UPC
+
+    private String valor;
+    private String tipo;
     private LocalDate fechaAsignacion;
     private String observaciones;
+
+    public CodigoBarras() {
+        super();
+    }
 
     public CodigoBarras(int id, String valor, String tipo, LocalDate fechaAsignacion, String observaciones) {
         super(id, false);
@@ -33,18 +19,6 @@ public class CodigoBarras extends Base {
         this.tipo = tipo;
         this.fechaAsignacion = fechaAsignacion;
         this.observaciones = observaciones;
-    }
-    
-    public CodigoBarras() { // Constructor por defecto para crear un codigo de barras nuevo sin ID.
-        super();
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
     }
 
     public String getValor() {
@@ -55,34 +29,37 @@ public class CodigoBarras extends Base {
         this.valor = valor;
     }
 
+    public String getTipo() {
+        return tipo;
+    }
 
-    @Override
-    public String toString() {
-        return "CodigoBarras{" +
-                "id=" + getId() +
-                ", tipo='" + tipo + '\'' +
-                ", valor='" + valor + '\'' +
-                ", fechaAsignacion='" + fechaAsignacion + '\'' +
-                ", observaciones='" + observaciones + '\'' +
-                ", eliminado=" + isEliminado() +
-                '}';
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
     public LocalDate getFechaAsignacion() {
         return fechaAsignacion;
     }
 
-    public String getObservaciones() {
-        return observaciones;
-    }
-
     public void setFechaAsignacion(LocalDate fechaAsignacion) {
         this.fechaAsignacion = fechaAsignacion;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
     }
 
     public void setObservaciones(String observaciones) {
         this.observaciones = observaciones;
     }
 
-    
+    @Override
+    public String toString() {
+        return "CodigoBarras [id=" + getId()
+                + ", tipo=" + tipo
+                + ", valor=" + valor
+                + ", fechaAsignacion=" + fechaAsignacion
+                + ", observaciones=" + observaciones
+                + ", eliminado=" + isEliminado() + "]";
+    }
 }
